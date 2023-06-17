6f8f97e3cd09522dd5f974c51b89689ed2b9bb7d
AakritiSedhai <secondshare007@gmail.com>
2023-06-17 13:58:20 +0545

    Updated Frontend

--- lib/view/HomeScreen.dart
+++ lib/view/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:ez_text/view/Widgets/chat_user_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -27,71 +28,83 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(30),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(110),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20),
+            ),
           ),
-        ),
-        leading: CircleAvatar(
-          radius: 20,
-        ),
-        centerTitle: true,
-        title: Text(
-          "EZText",
-          style: TextStyle(
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-            color: Colors.black,
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CircleAvatar(
+                radius: 30,
+                child: Icon(CupertinoIcons.person),
+              ),
+              SizedBox(width: 10),
+              Text(
+                "EZText",
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: IconButton(
-              onPressed: () {
-                // Handle search icon press
-              },
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: IconButton(
+                onPressed: () {
+                  // Handle search icon press
+                },
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: IconButton(
-              onPressed: () {
-                // Handle menu icon press
-              },
-              icon: Icon(
-                Icons.menu,
-                color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: IconButton(
+                onPressed: () {
+                  // Handle menu icon press
+                },
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
               ),
-            ),
-          ),
-        ],
-        bottom: TabBar(
-          controller: _tabController,
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey,
-          tabs: [
-            Tab(
-              text: 'Chats',
-
-            ),
-            Tab(
-              text: 'Status',
             ),
           ],
+          bottom: TabBar(
+            controller: _tabController,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(
+                text: 'Chats',
+              ),
+              Tab(
+                text: 'Status',
+              ),
+            ],
+          ),
         ),
       ),
       body: TabBarView(
         controller: _tabController,
         children: [
-          // Chats content
+          ListView.builder(
+              itemBuilder:(context,index){
+            return const ChatUserCard();
+          }),
           Container(
             color: Colors.blueAccent,
             child: Center(
